0a6f7d9ab27ee7517fbde369d60de55b96ee2cc4
Hirendra B <hirendrabalaji3@gmail.com>
2025-06-15 20:39:20 +0530

    added changes to GET

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Zhians Mood API is up and running! Use /api/log-entry');
+});
 
 // Mount the mood log routes
 app.use('/api', logRoutes);
