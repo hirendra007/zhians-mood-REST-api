dd9ec82aad723f95a0664f05557d288c296cb2da
Hirendra B <hirendrabalaji3@gmail.com>
2025-06-15 20:19:47 +0530

    making it production ready

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,3 @@ app.use(express.json());
 app.use('/api', logRoutes);
 
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
-});
